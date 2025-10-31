d42108dffa2b48c127c24c2bed491f37d424d799
Ricardo Delphino <radelphino@gmail.com>
2025-10-31 16:07:42 -0300

    fix redirect position

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { ScrollToTop } from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import AddToQueue from "./pages/AddToQueue";
 import Settings from "./pages/Settings";
@@ -20,6 +21,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <ScrollToTop />
           <Routes>
             <Route path="/:restaurantId" element={<Home />} />
             <Route path="/:restaurantId/add" element={<AddToQueue />} />
